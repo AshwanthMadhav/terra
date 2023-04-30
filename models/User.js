16c5c16f0e2c39d8785bb1451f15336ce3178356
Ashwanth Madhav <aswanthkmndr@gmail.com>
2023-04-30 14:53:19 +0530

    Added login and register

--- models/User.js
+++ models/User.js
@@ -18,7 +18,10 @@ const userSchema = new mongoose.Schema({
 
 function validateUser(user) {
   const schema = Joi.object({
-    email: Joi.string().email().required(),
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
     password: Joi.string().min(8).required(),
   });
 
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With"
+//   );
+//   next();
+// });
 
 const port = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-
-
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
